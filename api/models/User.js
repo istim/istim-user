455db910ecc524df2a08bc7c4c48f458f1cad817
Adelino Segundo <adelinosegundo@gmail.com>
2014-03-07 08:05:22 -0300

    added encript function to password

--- api/models/User.js
+++ api/models/User.js
@@ -1,5 +1,5 @@
 // Person.js
-var User = {
+module.exports = {
   attributes: {
     firstName: 'STRING',
     lastName: 'STRING',
@@ -16,7 +16,20 @@ var User = {
       minLength: 5,
       required: true
     }
+  },
+
+  beforeCreate: function (attrs, next) {
+      var bcrypt = require('bcrypt');
+
+      bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(attrs.password, salt, function(err, hash) {
+          if (err) return next(err);
+
+          attrs.password = hash;
+          next();
+        });
+      });
   }
 };
-
-module.exports = User;
