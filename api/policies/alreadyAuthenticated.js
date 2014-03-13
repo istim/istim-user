46f1969054a8b9d015b6cdec6cb5c0b96d21dbc3
Adelino Segundo <adelinosegundo@gmail.com>
2014-03-12 23:16:26 -0300

    fixed admin can create user validation

--- api/policies/alreadyAuthenticated.js
+++ api/policies/alreadyAuthenticated.js
@@ -12,14 +12,10 @@ module.exports = function(req, res, next) {
   if (req.originalUrl == '/login'){
   	return res.forbidden('You are already althenticated.');
   }
-
   User.findOne({ id: req.session.user }, function(err, user) {
-		if (user.is_admin){
-			return next();
-		}
+  	if (req.originalUrl == '/user/create' && !user.is_admin){
+  		return res.forbidden('You cant create users');
+  	}
   });
-  if (req.originalUrl == '/user/create'){
-  	return res.forbidden('You cant create users');
-  }
   return next();
 };
