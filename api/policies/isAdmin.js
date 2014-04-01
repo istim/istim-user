6125c24bfaaddf3fda8387b701de6f8f85df3d39
Adelino Segundo <adelinosegundo@gmail.com>
2014-04-01 08:49:37 -0300

    is_admin policie done

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -14,6 +14,7 @@ module.exports = function(req, res, next) {
   User.findOne({id: req.session.user, is_admin: true}).done(function(err, user){
   	if (err) return res.json('Unexpected system behavior', 500);
   	if (user) return next();
+  	else if (req.body.id == req.session.user) return next();
   	else return res.json('You are not permitted to perform this action', 403)
   })
   // User is not allowed
