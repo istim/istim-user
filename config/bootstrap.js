d5424c04fd8d9bf8d8c4ca256f8b7a83630fc747
Adelino Segundo <adelinosegundo@gmail.com>
2014-04-01 09:13:12 -0300

    Admin user auto-create done

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,5 +11,34 @@
 module.exports.bootstrap = function (cb) {
   // It's very important to trigger this callack method when you are finished 
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  User.findOne({is_admin: true}).done(function(err, user){
+    if (err) cb(); //DB error
+    //Admin exists
+    if (user) {
+        console.log('Admin already exists');
+        cb();
+    }
+    //admin do not exists
+    else User.create({ email: 'admin@istimuser.com', password: 'passadmin'}).done(function(err, user){
+        if (err) cb(); //DB Error
+        //User created
+        console.log('Creating admin user');
+        if (user) {
+            console.log('Giving admin user admin privileges');
+            User.createAdmin(user); //User now is Admin
+            cb();
+        } else User.findOneByEmail('admin@istimuser.com').done(function(err, user){
+            if (err) cb();
+            console.log('Admin user already exists');
+            if (user) {
+                console.log('Giving admin user admin privileges');
+                User.createAdmin(user); //User now is Admin
+                cb();
+            } else {
+                console.log('Everything else failed, just go on!');
+                cb();
+            };
+        });
+    });
+  });
 };
